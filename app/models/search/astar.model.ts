b34be8b59d5a59eebf7687d793c800420d4dc988
Ari <ari@Aris-MBP-2.fios-router.home>
2017-02-02 22:31:52 -0500

    search algorithm completed

--- app/models/search/astar.model.ts
+++ app/models/search/astar.model.ts
@@ -4,6 +4,11 @@ import { Direction } from '../cell.model';
 import { CellType} from '../cell.model';
 import { BinaryMinHeap} from '../binaryMinHeap.model';
 
+export enum searchType {
+    Astar,
+    WeightedAstar,
+    Uniformed
+}
 
 export class search {
 
@@ -17,15 +22,15 @@ export class search {
 	private openHeap: BinaryMinHeap<Cell>;
 	private closedHeap: BinaryMinHeap<Cell>;
 
+	private weight: number;
+
     constructor(grid: Grid, start: Cell, end: Cell) {
         this.grid = grid;
         this.start = start;
         this.end = end;
-        this.openList = new Array<Cell>();
         this.closedList = new Array<Cell>();
-
 		this.openHeap = new BinaryMinHeap<Cell>(cell => cell.id);
-		this.closedHeap = new BinaryMinHeap<Cell>(cell => cell.id);
+		this.weight = 2;
     }
 
 	doSearch(){
@@ -34,12 +39,12 @@ export class search {
 		this.openHeap.push(this.start, this.start.g);
 		while (this.openHeap.count > 0){
 			let currentNode = this.openHeap.pop();
-			if (currentNode = this.end){
-			let curr = currentNode;
-			let ret = new Array<Cell>();
-			while(curr.parent) {
-				ret.push(curr);
-				curr = curr.parent;
+			if(currentNode == this.end) {
+				let curr = currentNode;
+				let ret = new Array<Cell>();
+				while(curr.parent !== curr) {
+					ret.push(curr);
+					curr = curr.parent;
 				}
 				return ret.reverse();
 			}
@@ -53,13 +58,14 @@ export class search {
             	}
 			for(var i=0; i<neighbors.length;i++) {
 				let neighbor = neighbors[i];
-				if (this.closedList.indexOf(neighbor[1]) === -1){
-				if (this.openHeap.contains(neighbor[1])){
-				neighbor[1].g = Infinity;
-				neighbor[1].parent = null;
-			}
-			this.UpdateVertex(currentNode, neighbor);
-		}		
+				
+				if (this.closedList.indexOf(neighbor[1]) < 0){
+					if (this.openHeap.contains(neighbor[1])){
+						neighbor[1].g = Infinity;
+						neighbor[1].parent = null;
+					}
+				this.UpdateVertex(currentNode, neighbor);
+			}		
 
 		}
 
@@ -74,30 +80,26 @@ export class search {
 		if (this.openHeap.contains(neighbor[1])){
 			//this.openHeap.remove; 
 		}
-		this.openHeap.push(neighbor[1], neighbor[1].g + neighbor[1].h);
+		this.openHeap.push(neighbor[1], neighbor[1].g);
 
 		}
 	}
 
-    initiateSearch() {
-        // The set of nodes already evaluated.
-        // The set of currently discovered nodes that are already evaluated.
-        // Initially, only the start node is known.
+    initiateSearch(type: searchType) {
         this.start.parent = this.start;
         this.start.g = 0;
         this.start.f = this.start.h + this.start.g;
-		this.openHeap.push(this.start, this.start.g);
-        // For each node, which node it can most efficiently be reached from.
-        // If a node can be reached from many nodes, cameFrom will eventually contain the
-        // most efficient previous step.
+		let priority = 0;
+		if (type === searchType.Astar){
+			priority = this.start.g + this.start.h;
+		} else if (type === searchType.Uniformed){
+			priority = this.start.g;
+		} else if (type == searchType.WeightedAstar){
+			priority = this.start.g + this.start.h * this.weight;
+		}
+		this.openHeap.push(this.start, priority);
+
         while (this.openHeap.count > 0){
-            // Grab the lowest f(x) to process next
-		//	let lowInd = 0;
-		//	for(let i=0; i< this.openList.length; i++) {
-		//		if(this.openList[i].f < this.openList[lowInd].f) { 
-          //          lowInd = i; 
-            //    }
-		//	}
 			let currentNode = this.openHeap.pop();
  
 			// End case -- result has been found, return the traced path
@@ -111,9 +113,9 @@ export class search {
 				return ret.reverse();
 			}
 
-            // Normal case -- move currentNode from open to closed, process each of its neighbors
-			// this.openList.pop(); //!!!!!!
-			this.closedList.push(currentNode);
+			if (this.closedList.indexOf(currentNode) < 0){
+				this.closedList.push(currentNode);
+			}
             
             let neighbors = new Array<[Direction, Cell]>();
             let availableDirections = currentNode.availableDirections;
@@ -126,37 +128,30 @@ export class search {
 			for(var i=0; i<neighbors.length;i++) {
 				let neighbor = neighbors[i];
 
-				// g score is the shortest distance from start to current node, we need to check if
-				//	 the path we have arrived at this neighbor is the shortest one we have seen yet
-
 				if(this.closedList.indexOf(neighbor[1]) >= 0){
 					continue;
 				}
 				let gScore = currentNode.g + currentNode.getCost(neighbor[0]); 
-				let gScoreIsBest = false;
 				let beenVisited = neighbor[1].visited;
  
-				//if(this.closedList.indexOf(currentNode))
- 
-				/*if(this.openList.indexOf(neighbor[1]) == -1) {
-					// This the the first time we have arrived at this node, it must be the best
-					// Also, we need to take the h (heuristic) score since we haven't done so yet
-					gScoreIsBest = true;
-					//neighbor.h = astar.heuristic(neighbor.pos, end.pos);
-					this.openList.push(neighbor[1]);
-				}*/
 				if(!beenVisited || gScore < neighbor[1].g) {
-					// We have already seen the node, but last time it had a worse g (distance from start)
-					gScoreIsBest = true;
 					neighbor[1].parent = currentNode;
 					neighbor[1].visited = true;
-
 					neighbor[1].g = gScore;
 					neighbor[1].f = neighbor[1].g + neighbor[1].h;
+
 					console.log("neighbor : " , neighbor[1].id)
 					console.log("h : " , neighbor[1].h)
 					if(!beenVisited){
-						this.openHeap.push(neighbor[1], neighbor[1].g)
+						let priority = 0;
+						if (type === searchType.Astar){
+							priority = neighbor[1].g + neighbor[1].h;
+						} else if (type === searchType.Uniformed){
+							priority = neighbor[1].g;
+						} else if (type == searchType.WeightedAstar){
+							priority = neighbor[1].g + neighbor[1].h * this.weight;
+						}
+						this.openHeap.push(neighbor[1], priority)
 					} 
 				}
 			}
